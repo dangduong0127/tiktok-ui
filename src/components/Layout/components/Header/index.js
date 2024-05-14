597d5d5d0cc0ec0869cc3e2556868ffd7e361c82
DangDuong <dangduong0127@gmail.com>
2024-05-14 09:37:56 +0700

    HeaderUI

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,12 @@
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames/bind";
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +14,27 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/*Logo*/}
-        {/*search*/}
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+
+        <div className={cx("search")}>
+          <input
+            type="text"
+            spellCheck="false"
+            placeholder="Search account and video"
+          />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx("actions")}></div>
       </div>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "~/App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "~/components/GlobalStyles";
 //Fake comments
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyles>
